ae74ff4bcda29887349d2bf2a64e473f842744d2
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-05-09 14:15:35 -0400

    commit tree 34672b22c299bc636048367c2dcdb8ef2c19e1ad

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
+// function shout(string) {
+//     return string.toUpperCase();
+// }
+// function whisper(string) {
+//     return string.toLowerCase();
+// }
+
+// function logShout(string){
+//     console.log(string.toUpperCase())
+// }
+// function logWhisper(string){
+//     console.log(string.toLowerCase())
+// }
+
+// function sayHiToHeadphonedRoommate(string){
+//     if(string === string.toLowerCase()){
+//         return "I can't hear you!"
+//     }
+//     else if (string === string.toUpperCase()){
+//         return "YES INDEED!"
+//     }
+//     else if (string === "Let's have dinner together!"){
+//         return "I would love to!"
+//     }
+// }
+
 function shout(string) {
     return string.toUpperCase();
 }
-function whisper(string) {
+
+function whisper(string){
     return string.toLowerCase();
 }
-
-function logShout(string){
-    console.log(string.toUpperCase())
-}
-function logWhisper(string){
-    console.log(string.toLowerCase())
-}
-
-function sayHiToHeadphonedRoommate(string){
-    if(string === string.toLowerCase()){
-        return "I can't hear you!"
-    }
-    else if (string === string.toUpperCase()){
-        return "YES INDEED!"
-    }
-    else if (string === "Let's have dinner together!"){
-        return "I would love to!"
-    }
-}
+console.log(string.toUpperCase());
